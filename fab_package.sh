e01da2953d975510df69e50c106da12fe141cceb
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-08-01 12:42:39 +0300

    Package only the plugin

--- fab_package.sh
+++ fab_package.sh
@@ -1,15 +1,16 @@
-directories_to_keep="Config Content/Examples Plugins/SteamInputSimplePlugin/Config/\
- Plugins/SteamInputSimplePlugin/Content\
- Plugins/SteamInputSimplePlugin/Resources\
- Plugins/SteamInputSimplePlugin/Source\
- Plugins/SteamInputSimplePlugin/SteamInputSimplePlugin.uplugin\
- Source\
- SteamInputPlugin.uproject"
+cd Plugins
+directories_to_keep="SteamInputSimplePlugin/Config/\
+ SteamInputSimplePlugin/Content\
+ SteamInputSimplePlugin/Resources\
+ SteamInputSimplePlugin/Source\
+ SteamInputSimplePlugin/SteamInputSimplePlugin.uplugin"
 
 zip -r -9 -X -q -o "SteamInputPlugin.zip" $directories_to_keep
 
-[ -d target ] && rm -r target
+[ -d ../target ] && rm -r ../target
 
-mkdir -p target/fab
+mkdir -p ../target/fab
 
-mv "SteamInputPlugin.zip" target/fab/SteamInputPlugin.zip
+mv "SteamInputPlugin.zip" ../target/fab/SteamInputPlugin.zip
+
+cd -
